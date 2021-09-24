1ae9315c91bd411e16834d7217ed4534b98e1677
FoolHen <fulgen_3_7ad@hotmail.com>
2021-09-25 00:08:57 +0200

    feat(BundleLoader): fix bundle registry issues caused by randomized order

--- BundleLoader.lua
+++ BundleLoader.lua
@@ -48,30 +48,26 @@ function BundleLoader:OnLoadBundles(p_Hook, p_Bundles, p_Compartment)
 end
 
 function BundleLoader:OnRegisterEntityResources(p_LevelData)
-	m_Logger:Write("OnRegisterEntityResources")
-
-	if not m_BundleConfig then
-		return
-	end
-
-	m_Logger:Write("m_BundleConfig")
-
-	local s_RegistriesToLoad = m_BundleConfig.registryDataContainers or { }
-
-	m_Logger:Write("Registries:")
-	for k, _ in pairs(s_RegistriesToLoad) do
-		m_Logger:Write(k)
-	end
-
-	local s_Registry = RegistryContainer()
-
-	for l_RegistryName, l_RegistryDataContainer in pairs(s_RegistriesToLoad) do
+	local s_RegistriesToLoad = { 
+		-- 'SP_Tank_DesertFort_Registry'
+		DC(Guid('44234CB8-700B-461D-AF51-4FD9555128A7'), Guid('4C200C23-43D4-27E3-AC17-EBA1030EE457')),
+		-- 'Coop_006_Registry'
+		DC(Guid('23535E3D-E72F-11DF-99CA-879440EEBD7A'), Guid('51C54150-0ABF-03BD-EADE-1876AAD3EC8D')),
+		-- 'Coop_009_Registry'
+		DC(Guid('F94C5091-E69C-11DF-9B0E-AF9CA6E0236B'), Guid('F05798B2-31EC-210D-CC1D-0F7535BECA30')),
+		-- 'SP_Bank_Ride_SUB_Registry'
+		DC(Guid('8148A1BB-8F21-4E40-8A8F-2126000ABCD4'), Guid('9F9CABAF-21C2-EF4A-B35D-4358AEBA7565'))
+	}
+	
+	for l_Index, l_RegistryDataContainer in pairs(s_RegistriesToLoad) do
+		m_Logger:Write(l_Index)
 		local s_LoadedRegistry = l_RegistryDataContainer:GetInstance()
 
-		s_Registry.blueprintRegistry:add(s_LoadedRegistry)
+		if s_LoadedRegistry == nil then
+			m_Logger:Error("s_LoadedRegistry nil")
+		end
 
-		ResourceManager:AddRegistry(s_Registry, ResourceCompartment.ResourceCompartment_Game)
-		m_Logger:Write("Registry - " .. l_RegistryName)
+		ResourceManager:AddRegistry(s_LoadedRegistry, ResourceCompartment.ResourceCompartment_Game)
 	end
 end
 
