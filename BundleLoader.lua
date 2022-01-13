1fcb23bf5089a7f2dd6a56abb4b8ce77705a8b18
SassythSasqutch <sassythsasqutch@gmail.com>
2022-01-13 01:23:04 +0000

    Added a nil check for registry loading; added a couple registries.

--- BundleLoader.lua
+++ BundleLoader.lua
@@ -53,7 +53,7 @@ function BundleLoader:OnLoadBundles(p_Hook, p_Bundles, p_Compartment)
 end
 
 function BundleLoader:OnRegisterEntityResources(p_LevelData)
-	local s_RegistriesToLoad = {
+	local s_RegistriesToLoad = { 
 		-- 'SP_Tank_DesertFort_Registry'
 		DC(Guid('44234CB8-700B-461D-AF51-4FD9555128A7'), Guid('4C200C23-43D4-27E3-AC17-EBA1030EE457')),
 		-- 'Coop_006_Registry'
@@ -61,18 +61,22 @@ function BundleLoader:OnRegisterEntityResources(p_LevelData)
 		-- 'Coop_009_Registry'
 		DC(Guid('F94C5091-E69C-11DF-9B0E-AF9CA6E0236B'), Guid('F05798B2-31EC-210D-CC1D-0F7535BECA30')),
 		-- 'SP_Bank_Ride_SUB_Registry'
-		DC(Guid('8148A1BB-8F21-4E40-8A8F-2126000ABCD4'), Guid('9F9CABAF-21C2-EF4A-B35D-4358AEBA7565'))
+		DC(Guid('8148A1BB-8F21-4E40-8A8F-2126000ABCD4'), Guid('9F9CABAF-21C2-EF4A-B35D-4358AEBA7565')),
+		-- 'MP_017_R_Registry'
+		DC(Guid('D9E8DC6C-250E-4AF9-9878-07809B5AE5D9'), Guid('33FF3424-F0C5-9A3A-5C0F-A49573416A13')),
+		-- 'MP_013_ConquestLarge_Registry'
+		DC(Guid('1CEC6C7E-1629-4631-B326-1A134BC6EF27'), Guid('6196137B-50D6-4607-98AE-900BACF47065')),
 	}
-
+	
 	for l_Index, l_RegistryDataContainer in pairs(s_RegistriesToLoad) do
 		m_Logger:Write(l_Index)
 		local s_LoadedRegistry = l_RegistryDataContainer:GetInstance()
 
 		if s_LoadedRegistry == nil then
-			m_Logger:Error("s_LoadedRegistry nil")
+			m_Logger:Warning("s_LoadedRegistry nil")
+		else
+			ResourceManager:AddRegistry(s_LoadedRegistry, ResourceCompartment.ResourceCompartment_Game)
 		end
-
-		ResourceManager:AddRegistry(s_LoadedRegistry, ResourceCompartment.ResourceCompartment_Game)
 	end
 end
 
