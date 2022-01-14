eaad32559a6a6222d3dd950b70a020419f051192
3ti65 <st3ti65@gmail.com>
2022-01-14 01:00:06 +0100

    feat(xp5_004): add bundle config

---
+++ LevelBundleConfigs/XP5_004_ConquestLarge0.BundleConfig.lua
@@ -0,0 +1,22 @@
+return {
+	terrainAssetName = 'levels/XP5_004/XP5_004_Terrain/XP5_004_Terrain',
+	superBundles = {
+		'SpChunks',
+		'Levels/SP_Tank/SP_Tank',
+		'Levels/SP_Bank/SP_Bank',
+		'Levels/Coop_006/Coop_006',
+		'Levels/Coop_009/Coop_009',
+		-- 'Xp5Chunks',
+		-- 'Levels/XP5_001/XP5_001',
+	},
+	bundles = {
+		'Levels/SP_Tank/SP_Tank',
+		'Levels/SP_Tank/DesertFort',
+		'Levels/SP_Bank/SP_Bank',
+		'Levels/SP_Bank/Ride_SUB',
+		'Levels/Coop_006/Coop_006',
+		'Levels/Coop_009/Coop_009',
+		-- 'Levels/XP5_001/XP5_001',
+		-- 'Levels/XP5_001/Air_Superiority',
+	}
+}
