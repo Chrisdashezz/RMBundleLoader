efcd858801745ff930b5d3e72b4925aa64ad6a7f
SassythSasqutch <37040523+SassythSasqutch@users.noreply.github.com>
2022-01-18 17:41:22 +0000

    fix(sp_tank): improve server launch stability by re-ordering bundles

--- LevelBundleConfigs/SP_Tank_ConquestLarge0.BundleConfig.lua
+++ LevelBundleConfigs/SP_Tank_ConquestLarge0.BundleConfig.lua
@@ -1,27 +1,27 @@
 return {
     terrainAssetName = 'Levels/SP_Tank/Terrain/SP_Tank_Terrain_02',
     superBundles = {
-        'Levels/Coop_006/Coop_006',
-        'Levels/Coop_009/Coop_009',
-        'Levels/MP_013/MP_013', -- HMMWV/Vodnik, mapping assets
         -- For SP terrain loading
         'ui',
         'mpchunks',
-        'levels/sp_tank/sp_tank', -- This is not a mistake. You have to manually add these bundles unless the game crashes when you want to spawn a blueprint from it.
         'levels/mp_012/mp_012', -- MP logic
+        -- RM        
+        'Levels/Coop_006/Coop_006',
+        'Levels/Coop_009/Coop_009',
+        'Levels/MP_013/MP_013', -- HMMWV/Vodnik, mapping assets
     },
     bundles = {
+        -- For SP terrain loading
+        'levels/sp_tank/desertfort',
+        'levels/sp_tank/backdrop',
+        'levels/sp_tank/miclicart',
+        'ui/flow/bundle/loadingbundlemp',
+        'levels/mp_012/mp_012',
+        'levels/mp_012/conquest_large',
+        -- RM        
         'Levels/Coop_006/Coop_006',
         'Levels/Coop_009/Coop_009',
         'Levels/MP_013/MP_013',
         'Levels/MP_013/ConquestLarge',
-        -- For SP terrain loading
-        'ui/flow/bundle/loadingbundlemp',
-        'levels/sp_tank/sp_tank',
-        'levels/sp_tank/desertfort',
-        'levels/sp_tank/backdrop',
-        'levels/sp_tank/miclicart',
-        'levels/mp_012/mp_012',
-        'levels/mp_012/conquest_large',
     }
 }
