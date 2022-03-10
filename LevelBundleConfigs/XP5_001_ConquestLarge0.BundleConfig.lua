177bb9b32a9754012333768837363fdb91ced4bc
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-03-10 11:49:53 +0100

    refactor(BundleLoader): add common bundleconfig

--- LevelBundleConfigs/XP5_001_ConquestLarge0.BundleConfig.lua
+++ LevelBundleConfigs/XP5_001_ConquestLarge0.BundleConfig.lua
@@ -1,22 +1,5 @@
 return {
 	terrainAssetName = 'levels/XP5_001/terrain/terrainexample',
-	superBundles = {
-		'SpChunks',
-		'Levels/SP_Tank/SP_Tank',
-		'Levels/SP_Bank/SP_Bank',
-		'Levels/Coop_006/Coop_006',
-		'Levels/Coop_009/Coop_009',
-		-- 'Xp5Chunks',
-		-- 'Levels/XP5_001/XP5_001',
-	},
-	bundles = {
-		'Levels/SP_Tank/SP_Tank',
-		'Levels/SP_Tank/DesertFort',
-		'Levels/SP_Bank/SP_Bank',
-		'Levels/SP_Bank/Ride_SUB',
-		'Levels/Coop_006/Coop_006',
-		'Levels/Coop_009/Coop_009',
-		-- 'Levels/XP5_001/XP5_001',
-		-- 'Levels/XP5_001/Air_Superiority',
-	}
+	superBundles = {},
+	bundles = {}
 }
