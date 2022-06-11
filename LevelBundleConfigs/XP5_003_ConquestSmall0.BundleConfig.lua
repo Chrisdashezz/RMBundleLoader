f23b8fc09f0e3b4eab607b5d676e26412e69cc63
Joe Constable <joeconstable97@gmail.com>
2022-06-11 11:23:59 +0100

    feat(alt_map_layouts): add XP5_001, 03 and 04

---
+++ LevelBundleConfigs/XP5_003_ConquestSmall0.BundleConfig.lua
@@ -0,0 +1,15 @@
+return {
+	terrainAssetName = 'Levels/XP5_003/XP5_003_Terrain/XP5_003_Terrain',
+	superBundles = {
+		'levels/mp_013/mp_013'
+	},
+	bundles = {
+		'levels/mp_013/mp_013'
+	},
+	blueprintGuidsToBlock = {
+		-- Architecture/ME_House01/ME_House01_Garage_Destruction
+		["4FD89229-7326-6148-92C3-AA6750C1BC2C"] = true,
+		-- Architecture/StoreRoom_01/StoreRoom_01_Destruction/
+		["A29933D7-01C5-4934-0033-D639EEA3180D"] = true,
+	}
+}
