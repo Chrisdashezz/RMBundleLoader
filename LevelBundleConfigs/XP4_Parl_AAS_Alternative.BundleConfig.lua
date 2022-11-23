f7147de3e285598a753607348bb289b1c07bfe0d
Cameron Basham <cameron250taylor@outlook.com>
2022-11-23 18:34:50 -0500

    feature(custom-gamemode): rename

---
+++ LevelBundleConfigs/XP4_Parl_AAS_Alternative.BundleConfig.lua
@@ -0,0 +1,9 @@
+return {
+	terrainAssetName = 'Levels/XP4_Parliament/Terrain_Parliament/Terrain_Parliament',
+	superBundles = {
+		'Levels/Coop_010/Coop_010'
+	},
+	bundles = {
+		'Levels/Coop_010/Coop_010'
+	}
+}
