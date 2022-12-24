a68809a077710f54139ac2bcc07321e0cb7beece
Cameron Basham <cameron250taylor@outlook.com>
2022-12-24 01:11:16 -0500

    feature(custom-gamemodes): Added Bandar BC
    Added XP3_Desert BundelConfig

---
+++ LevelBundleConfigs/XP3_Desert_AAS_Standard.BundleConfig.lua
@@ -0,0 +1,12 @@
+return {
+	terrainAssetName = 'Levels/XP3_Desert/Terrain/Terrain_8k',
+	superBundles = {
+		'Levels/XP3_Shield/XP3_Shield',
+		'Levels/MP_012/MP_012',
+	},
+	bundles = {
+		'Levels/XP3_Shield/XP3_Shield',
+		'Levels/MP_012/MP_012',
+	},
+	blueprintGuidsToBlock = {}
+}
