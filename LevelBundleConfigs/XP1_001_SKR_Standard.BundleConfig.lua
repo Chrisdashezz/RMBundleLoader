37963b9bb36ee3131cb21ce50f0baba38b7e5dd5
FoolHen <fulgen_3_7ad@hotmail.com>
2023-01-15 16:22:45 +0100

    feat: configs now support multiple gamemodes avoiding duplicated files

--- LevelBundleConfigs/XP1_001_SKR_Standard.BundleConfig.lua
+++
@@ -1,9 +0,0 @@
-return {
-	terrainAssetName = 'Levels/XP1_001/XPACK1_001_Terrain',
-	superBundles = {
-		'Levels/Coop_010/Coop_010'
-	},
-	bundles = {
-		'Levels/Coop_010/Coop_010'
-	}
-}
---
+++ LevelBundleConfigs/XP5_003.BundleConfig.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		gamemodes = {
+			"ASS_Standard",
+			"ASS_Alternate",
+			"ConquestLarge0"
+		},
+		config = {
+			terrainAssetName = 'Levels/XP5_003/XP5_003_Terrain/XP5_003_Terrain',
+			superBundles = {
+				'Levels/MP_013/MP_013'
+			},
+			bundles = {
+				'Levels/MP_013/MP_013'
+			},
+			dynamicBundles = {
+				'Levels/MP_013/ConquestLarge'
+			},
+			dynamicRegistries = {
+				-- 'MP_013_ConquestLarge_Registry'
+				DC(Guid('1CEC6C7E-1629-4631-B326-1A134BC6EF27'), Guid('6196137B-50D6-4607-98AE-900BACF47065'))
+			},
+			blueprintGuidsToBlock = {
+				-- Architecture/ME_House01/ME_House01_Garage_Destruction
+				["4FD89229-7326-6148-92C3-AA6750C1BC2C"] = true,
+				-- Architecture/StoreRoom_01/StoreRoom_01_Destruction/
+				["A29933D7-01C5-4934-0033-D639EEA3180D"] = true
+			}
+		}
+	},
+	{
+		terrainAssetName = 'Levels/XP5_003/XP5_003_Terrain/XP5_003_Terrain',
+		superBundles = {
+			'levels/mp_013/mp_013'
+		},
+		bundles = {
+			'levels/mp_013/mp_013'
+		},
+		blueprintGuidsToBlock = {
+			-- Architecture/ME_House01/ME_House01_Garage_Destruction
+			["4FD89229-7326-6148-92C3-AA6750C1BC2C"] = true,
+			-- Architecture/StoreRoom_01/StoreRoom_01_Destruction/
+			["A29933D7-01C5-4934-0033-D639EEA3180D"] = true,
+		}
+	}
+}
