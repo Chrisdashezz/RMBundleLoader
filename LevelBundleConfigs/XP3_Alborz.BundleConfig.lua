a429562e96bd916739ef69003057fa8baf4296f7
Chrisdashezz <108307974+Chrisdashezz@users.noreply.github.com>
2023-03-15 19:51:41 +0400

    Feature(Bundles): Add Alborz Bundle

---
+++ LevelBundleConfigs/XP3_Alborz.BundleConfig.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		gamemodes = {
+			"SKR Standard",
+			"ConquestLarge0"
+		},
+		config = {
+			terrainAssetName = 'Levels/MP_WhitePeak/Terrain/Terrain',
+			superBundles = {
+				'levels/MP_017/MP_017',
+				'Levels/XP3_Alborz/XP3_Alborz'
+			},
+			bundles = {
+				'levels/MP_017/MP_017',
+				'Levels/XP3_Alborz/XP3_Alborz'
+				
+			},
+		}
+	}
+}
