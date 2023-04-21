663ef00f5a3fe4ba5131efe69515fce4446f84f0
Chrisdashezz <108307974+Chrisdashezz@users.noreply.github.com>
2023-04-21 13:55:19 +0400

    Create XP3_Alborz.BundleConfig.lua

---
+++ LevelBundleConfigs/XP3_Alborz.BundleConfig.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		gamemodes = {
+			"AdvanceAndSecureStd",
+			"AdvanceAndSecureAlt",
+			"ConquestLarge0"
+		},
+		config = {
+			terrainAssetName = 'Levels/MP_WhitePeak/Terrain/Terrain',
+			superBundles = {
+				'levels/MP_017/MP_017',
+				'Levels/XP3_Alborz/XP3_Alborz'
+			},
+			bundles = {
+				'levels/MP_017/MP_017',
+				'Levels/XP3_Alborz/XP3_Alborz'
+				
+			},
+		}
+	}
+}
